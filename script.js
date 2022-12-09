ceb45ed47a1e2851737aa74fd51eb5df1f81c488
ivanvk86 <ivanvk86@gmail.com>
2022-12-09 09:37:04 -0300

    script editing

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ function cambiar_parrafo() {
     console.log(texto);
   };
 
-  function myFunction2(valor) {
+  function mostrar_texto(valor) {
     document.getElementById("text-acercade").innerText=valor;
   };
 
   let textarea = document.getElementById("edit-acercade");
   textarea.addEventListener('keyup', (e) => {
-    logMessage(`Key "${e.key}" released [event:keyup]`);
+    console.log(`Key "${e.key}" released [event:keyup]`);
     if (e.key=="Enter"){
-      document.getElementById("edit-acercade").style.display="none"
+      document.getElementById("edit-acercade").style.display="none";
     }
   });
 
